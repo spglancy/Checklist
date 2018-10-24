02561743ff0ceaa74470e9576d6c795e2203f2c4
Sean <Spglancy@gmail.com>
2018-10-24 16:15:03 -0700

    added lowercase support

--- checklist.py
+++ checklist.py
@@ -19,48 +19,43 @@ def listAllItems():
         print(str(index) + ". " + listItem)
         index += 1
 
+def exit():
+    running = false
+
 def markComplete(index):
     original = str(checklist[int(index)])
     update(int(index), "√" + original)
 
 def test():
-    create("purple sox")
-    create("red cloak")
-
-    print(read(0))
-    print(read(1))
-
-    update(0, "purple socks")
-    destroy(1)
-
-    print(read(0))
+    running = True
+    while running:
+        selection = input("Press C to add to list, R to Read from list, D to mark done, Q to quit, and P to display list \n")
+        select(selection)
 
 def select(function_code):
     # Create item
-    if function_code == "C":
+    if function_code == "C" or function_code == "c":
         input_item = input("Input item: ")
         create(input_item)
 
+    if function_code == "Q" or function_code == "q":
+        quit()
     # Read item
-    elif function_code == "R":
+elif function_code == "R" or function_code == "r":
         item_index = input("Index Number: ")
 
         # Remember that item_index must actually exist or our program will crash.
         print(read(int(item_index)))
 
     # Print all items
-    elif function_code == "P":
+elif function_code == "P" or function_code == "p":
         listAllItems()
 
-    elif function_code == "D":
+    elif function_code == "D" or function_code =="d":
         markComplete(input("Index Number: "))
 
     # Catch all
     else:
         print("Unknown Option")
 
-running = True
-while running:
-    selection = input(
-        "Press C to add to list, R to Read from list, D to mark done, and P to display list \n")
-    select(selection)
+test()
